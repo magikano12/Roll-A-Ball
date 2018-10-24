529b45df365fbede6101f22b54dd0aad9d2ca26a
magikano1 <joshua.rogers@loop.colum.edu>
2018-10-24 01:21:44 -0500

    Camera and Collectables
    - adjusted camera angle
    -created moving collectables
    -made play area

--- Roll A Ball/Assets/Scripts/Playercontroller.cs
+++ Roll A Ball/Assets/Scripts/Playercontroller.cs
@@ -1,22 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Playercontroller : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
 	// Use this for initialization
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		
-	}
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
 
     void FixedUpdate()
     {
@@ -26,5 +28,23 @@ public class Playercontroller : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
